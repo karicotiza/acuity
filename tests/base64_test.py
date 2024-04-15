ac2b59b2399eba8986a2be3d64209f4c07746269
Danial Kurtumerov <kur7umerovd@gmail.com>
2024-04-15 10:44:04 +0500

    tests: recognition endpoints
    Added new HTTP methods, checking their status
    codes and content.

--- tests/base64_test.py
+++ tests/base64_test.py
@@ -2,13 +2,94 @@ import requests
 import pathlib
 
 
-def test_base64_recognition() -> None:
-    url: str = 'http://localhost:8000/api/v1/base64/'
-    file: pathlib.Path = pathlib.Path('tests', 'base64.txt')
-    payload: dict = {'base64': open(file, 'r', encoding='utf-8')}
+URL: str = 'http://localhost:8000/api/v1/base64/'
+FILE: pathlib.Path = pathlib.Path('tests', 'base64.txt')
+PAYLOAD: dict = {'base64': open(FILE, 'r', encoding='utf-8')}
+TEXT: str = 'hano world'
+OPTIONS: dict = {
+    "name": "Base64 List",
+    "description": "",
+    "renders": [
+        "application/json",
+        "text/html"
+    ],
+    "parses": [
+        "application/json",
+        "application/x-www-form-urlencoded",
+        "multipart/form-data"
+    ],
+    "actions": {
+        "POST": {
+            "base64": {
+                "type": "string",
+                "required": True,
+                "read_only": False,
+                "label": "Base64"
+            }
+        }
+    }
+}
+WRONG_FILE: pathlib.Path = pathlib.Path('tests', 'audio.wav')
+WRONG_PAYLOAD: dict = {'file': open(WRONG_FILE, 'rb')}
 
-    response: requests.Response = requests.post(url=url, data=payload)
+
+def test_get_405() -> None:
+    response: requests.Response = requests.get(URL)
+    data: dict = response.json()
+
+    assert response.status_code == 405
+    assert data.get('detail', '') == 'Method "GET" not allowed.'
+
+
+def test_head_405() -> None:
+    response: requests.Response = requests.head(URL)
+
+    assert response.status_code == 405
+
+
+def test_post_201() -> None:
+    response: requests.Response = requests.post(URL, PAYLOAD)
     data: dict = response.json()
 
     assert response.status_code == 201
-    assert data.get('status') == 'hano world'
+    assert data.get('text', '') == TEXT
+
+
+def test_post_400() -> None:
+    response: requests.Response = requests.post(URL, files=WRONG_PAYLOAD)
+    data: dict = response.json()
+
+    assert response.status_code == 400
+    assert data == {'base64': ['This field is required.']}
+
+
+def test_put_405() -> None:
+    response: requests.Response = requests.put(URL)
+    data: dict = response.json()
+
+    assert response.status_code == 405
+    assert data.get('detail', '') == 'Method "PUT" not allowed.'
+
+
+def test_patch_405() -> None:
+    response: requests.Response = requests.patch(URL)
+    data: dict = response.json()
+
+    assert response.status_code == 405
+    assert data.get('detail', '') == 'Method "PATCH" not allowed.'
+
+
+def test_delete_405() -> None:
+    response: requests.Response = requests.delete(URL)
+    data: dict = response.json()
+
+    assert response.status_code == 405
+    assert data.get('detail', '') == 'Method "DELETE" not allowed.'
+
+
+def test_options_200() -> None:
+    response: requests.Response = requests.options(URL)
+    data: dict = response.json()
+
+    assert response.status_code == 200
+    assert data == OPTIONS
--- tests/file_test.py
+++ tests/file_test.py
@@ -2,13 +2,94 @@ import requests
 import pathlib
 
 
-def test_file_recognition() -> None:
-    url: str = 'http://localhost:8000/api/v1/file/'
-    file: pathlib.Path = pathlib.Path('tests', 'audio.wav')
-    files: dict = {'file': open(file, 'rb')}
+URL: str = 'http://localhost:8000/api/v1/file/'
+FILE: pathlib.Path = pathlib.Path('tests', 'audio.wav')
+PAYLOAD: dict = {'file': open(FILE, 'rb')}
+TEXT: str = 'hano world'
+OPTIONS: dict = {
+    "name": "File List",
+    "description": "",
+    "renders": [
+        "application/json",
+        "text/html"
+    ],
+    "parses": [
+        "application/json",
+        "application/x-www-form-urlencoded",
+        "multipart/form-data"
+    ],
+    "actions": {
+        "POST": {
+            "file": {
+                "type": "file upload",
+                "required": True,
+                "read_only": False,
+                "label": "File"
+            }
+        }
+    }
+}
+WRONG_FILE: pathlib.Path = pathlib.Path('tests', 'base64.txt')
+WRONG_PAYLOAD: dict = {'base64': open(WRONG_FILE, 'r', encoding='utf-8')}
 
-    response: requests.Response = requests.post(url=url, files=files)
+
+def test_get_405() -> None:
+    response: requests.Response = requests.get(URL)
+    data: dict = response.json()
+
+    assert response.status_code == 405
+    assert data.get('detail', '') == 'Method "GET" not allowed.'
+
+
+def test_head_405() -> None:
+    response: requests.Response = requests.head(URL)
+
+    assert response.status_code == 405
+
+
+def test_post_201() -> None:
+    response: requests.Response = requests.post(URL, files=PAYLOAD)
     data: dict = response.json()
 
     assert response.status_code == 201
-    assert data.get('status') == 'hano world'
+    assert data.get('text', '') == 'hano world'
+
+
+def test_post_400() -> None:
+    response: requests.Response = requests.post(URL, files=WRONG_PAYLOAD)
+    data: dict = response.json()
+
+    assert response.status_code == 400
+    assert data == {'file': ['No file was submitted.']}
+
+
+def test_put_405() -> None:
+    response: requests.Response = requests.put(URL)
+    data: dict = response.json()
+
+    assert response.status_code == 405
+    assert data.get('detail', '') == 'Method "PUT" not allowed.'
+
+
+def test_patch_405() -> None:
+    response: requests.Response = requests.patch(URL)
+    data: dict = response.json()
+
+    assert response.status_code == 405
+    assert data.get('detail', '') == 'Method "PATCH" not allowed.'
+
+
+def test_delete_405() -> None:
+    response: requests.Response = requests.delete(URL)
+    data: dict = response.json()
+
+    assert response.status_code == 405
+    assert data.get('detail', '') == 'Method "DELETE" not allowed.'
+
+
+def test_options_200() -> None:
+    response: requests.Response = requests.options(URL)
+    data: dict = response.json()
+
+    assert response.status_code == 200
+    assert data == OPTIONS
