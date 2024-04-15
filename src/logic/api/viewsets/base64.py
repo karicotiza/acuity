9a49a1eb6b060f86d6fff4dc2634a83ebd948d0d
Danial Kurtumerov <kur7umerovd@gmail.com>
2024-04-15 10:48:00 +0500

    refactor: recognition endpoints
    Added new serializers. Explicitly specified
    serializers to generate api documentation.

--- src/logic/api/viewsets/base64.py
+++ src/logic/api/viewsets/base64.py
@@ -3,13 +3,19 @@ import typing
 from rest_framework import viewsets, status, mixins
 from rest_framework.serializers import BaseSerializer
 from rest_framework.response import Response
+from drf_spectacular.utils import extend_schema
 from logic.api.serializers.base64 import Base64Serializer
+from logic.api.serializers.text import TextSerializer
 from logic.models import AudioData, Base64
 
 
 class Base64ViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin):
     serializer_class: typing.Type[Base64Serializer] = Base64Serializer
 
+    @extend_schema(
+        request=Base64Serializer,
+        responses={201: TextSerializer},
+    )
     def create(self, request, *args, **kwargs) -> Response:
         post: BaseSerializer = self.get_serializer(data=request.data)
         post.is_valid(raise_exception=True)
@@ -19,10 +25,14 @@ class Base64ViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin):
         base64_: Base64 = Base64(base64)
         audio_data: AudioData = AudioData(base64_.to_bytes())
 
-        data: dict = {'status': audio_data.recognize()}
+        response_data: TextSerializer = TextSerializer(
+            data={'text': audio_data.recognize()}
+        )
+
+        response_data.is_valid()
 
         response: Response = Response(
-            data=data,
+            data=response_data.validated_data,
             status=status.HTTP_201_CREATED,
             headers=headers
         )
--- src/logic/api/viewsets/file.py
+++ src/logic/api/viewsets/file.py
@@ -4,13 +4,19 @@ from django.core.files.uploadedfile import InMemoryUploadedFile
 from rest_framework import viewsets, status, mixins
 from rest_framework.serializers import BaseSerializer
 from rest_framework.response import Response
+from drf_spectacular.utils import extend_schema
 from logic.api.serializers.file import FileSerializer
+from logic.api.serializers.text import TextSerializer
 from logic.models import AudioData, BytesIO
 
 
 class FileViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin):
     serializer_class: typing.Type[FileSerializer] = FileSerializer
 
+    @extend_schema(
+        request=FileSerializer,
+        responses={201: TextSerializer},
+    )
     def create(self, request, *args, **kwargs) -> Response:
         post: BaseSerializer = self.get_serializer(data=request.data)
         post.is_valid(raise_exception=True)
@@ -25,10 +31,14 @@ class FileViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin):
 
         audio_data: AudioData = AudioData(bytes_)
 
-        data: dict = {'status': audio_data.recognize()}
+        response_data: TextSerializer = TextSerializer(
+            data={'text': audio_data.recognize()}
+        )
+
+        response_data.is_valid()
 
         response: Response = Response(
-            data=data,
+            data=response_data.validated_data,
             status=status.HTTP_201_CREATED,
             headers=headers
         )
