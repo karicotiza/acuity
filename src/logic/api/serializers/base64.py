9859c566b4485c1d39bb71e33a9f3edf4de19e8b
Danial Kurtumerov <kur7umerovd@gmail.com>
2024-04-16 17:10:11 +0500

    feat: validation
    Added logic to the serializers for data
    validation, followed by exception generation
    and response generation.

--- src/logic/api/serializers/base64.py
+++ src/logic/api/serializers/base64.py
@@ -1,5 +1,32 @@
+from io import BytesIO
+from base64 import b64decode, b64encode
 from rest_framework import serializers
+from logic.models import Base64
+from pydub import AudioSegment  # type: ignore
 
 
 class Base64Serializer(serializers.Serializer):
     base64: serializers.CharField = serializers.CharField()
+
+    def validate_base64(self, data: str) -> str:
+        try:
+            decoded_bytes: bytes = b64decode(data)
+            encoded_bytes: bytes = b64encode(decoded_bytes)
+            converted: str = str(encoded_bytes)
+            converted = converted[2:-1]
+
+            if data != converted:
+                raise serializers.ValidationError('Malformed base64 data.')
+
+        except Exception:
+            raise serializers.ValidationError('Malformed base64 data.')
+
+        try:
+            base64_: Base64 = Base64(data)
+            bytes_io: BytesIO = BytesIO(base64_.to_bytes())
+
+            AudioSegment.from_file(bytes_io)
+        except IndexError:
+            raise serializers.ValidationError('Not an audio.')
+
+        return data
--- src/logic/api/serializers/file.py
+++ src/logic/api/serializers/file.py
@@ -1,5 +1,27 @@
+from io import BytesIO
+from typing import Any
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from rest_framework import serializers
+from pydub import AudioSegment  # type: ignore
 
 
 class FileSerializer(serializers.Serializer):
     file: serializers.FileField = serializers.FileField()
+
+    def validate_file(
+        self, file: InMemoryUploadedFile
+    ) -> InMemoryUploadedFile:
+        try:
+            bytes_io: BytesIO | Any = file.file
+
+            if isinstance(bytes_io, BytesIO):
+                bytes_: bytes = bytes_io.read()
+                bytes_io.seek(0)
+
+            bytes_io_: BytesIO = BytesIO(bytes_)
+
+            AudioSegment.from_file(bytes_io_)
+        except Exception:
+            raise serializers.ValidationError('Not an audio file.')
+
+        return file
