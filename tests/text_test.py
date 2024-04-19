4bcd01b25488ed80155e24a408a8e3c4c80d31cf
Danial Kurtumerov <kur7umerovd@gmail.com>
2024-04-19 16:12:30 +0500

    tests: recognition endpoints
    Added new tests to check the validation of non
    existent recognition links.

--- tests/text_test.py
+++ tests/text_test.py
@@ -1,10 +1,17 @@
 import requests
+import pathlib
 
+
+POST_URL: str = 'http://localhost:8000/api/v1/file/'
 URL_LIST: str = 'http://localhost:8000/api/v1/text/'
 URL_DETAIL = (
     URL_LIST +
-    '10e0e1c3828817839a450f9895c85e975122801e27f65faee4cdb7d588b8e305/'
-) 
+    '337096cb374d208ebf4deae2eb365956c090ccfb1c9b7a35bda7874a803eb648/'
+)
+WRONG_URL_DETAIL = (
+    URL_LIST +
+    '1/'
+)
 TEXT: str = 'hano world'
 OPTIONS: dict = {
     "name": "Text Instance",
@@ -21,13 +28,21 @@ OPTIONS: dict = {
 }
 
 
+def payload(name: str):
+    file: pathlib.Path = pathlib.Path('tests', 'data', 'audio', name)
+    payload: dict = {'file': open(file, 'rb')}
+
+    return payload
+
+
 def test_get_list_404() -> None:
     response: requests.Response = requests.get(URL_LIST)
 
     assert response.status_code == 404
 
 
-def test_get_detail_404() -> None:
+def test_get_detail_200() -> None:
+    requests.post(POST_URL, files=payload('audio.aac'))
     response: requests.Response = requests.get(URL_DETAIL)
     data: dict = response.json()
 
@@ -36,7 +51,13 @@ def test_get_detail_404() -> None:
     assert 'text' in data.keys()
 
 
+def test_get_detail_404() -> None:
+    response: requests.Response = requests.get(WRONG_URL_DETAIL)
+    assert response.status_code == 404
+
+
 def test_head_detail_200() -> None:
+    requests.post(POST_URL, files=payload('audio.aac'))
     response: requests.Response = requests.head(URL_DETAIL)
 
     assert response.status_code == 200
