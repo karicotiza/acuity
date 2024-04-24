2cd5c45cd13d6b9767e03f93762af8c0e6799ed1
Danial Kurtumerov <kur7umerovd@gmail.com>
2024-04-24 10:53:00 +0500

    chore: updated init.sh
    Removed the command to create migrations in the
    file init.sh Since migrations are created during
    local development and monitored in the version
    control system, that is, fresh migrations are
    always sent to the deployment environment, and
    this command does not change anything.

--- src/init.sh
+++ src/init.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-python manage.py makemigrations logic && \
 python manage.py migrate && \
 python manage.py collectstatic --noinput && \
 python manage.py createsuperuser --noinput || true && \
