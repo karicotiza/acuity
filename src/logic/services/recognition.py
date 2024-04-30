dde2f6a63c1f5e1141daa3cef1c1f5c42eed3042
Danial Kurtumerov <kur7umerovd@gmail.com>
2024-04-30 10:29:52 +0500

    feat: gpu recognition
    Added a new class that uses cuda for recognition.

--- src/logic/services/recognition.py
+++ src/logic/services/recognition.py
@@ -15,7 +15,7 @@ class IRecognitionModel:
         raise NotImplementedError
 
 
-class XLSR53(IRecognitionModel):
+class XLSR53_CPU(IRecognitionModel):
     __necessary_files: list[str] = [
         'config.json',
         'preprocessor_config.json',
@@ -33,8 +33,8 @@ class XLSR53(IRecognitionModel):
 
         self.__validate()
 
-        self.__processor = Wav2Vec2Processor.from_pretrained(path)
-        self.__model = Wav2Vec2ForCTC.from_pretrained(path)
+        self.__model = Wav2Vec2ForCTC.from_pretrained(self.__path)
+        self.__processor = Wav2Vec2Processor.from_pretrained(self.__path)
         self.__sample_rate = sample_rate
 
     def __validate(self) -> None:
@@ -77,6 +77,86 @@ class XLSR53(IRecognitionModel):
         with torch.no_grad():
             outputs: CausalLMOutput = self.__model(
                 inputs.input_values, attention_mask=inputs.attention_mask,
+            )
+
+            logits: torch.Tensor = outputs.logits
+
+        ids: torch.Tensor = torch.argmax(logits, dim=-1)
+        predicted_sentences: list = self.__processor.batch_decode(ids)
+
+        if predicted_sentences:
+            return predicted_sentences[0]
+
+        else:
+            return ''
+
+
+class XLSR53_GPU(IRecognitionModel):
+    __necessary_files: list[str] = [
+        'config.json',
+        'preprocessor_config.json',
+        'pytorch_model.bin',
+        'vocab.json',
+    ]
+
+    __link: str = (
+        'https://huggingface.co/jonatasgrosman/' +
+        'wav2vec2-large-xlsr-53-english/tree/main'
+    )
+
+    def __init__(self, path: Path, sample_rate: int) -> None:
+        self.__path = path
+
+        self.__validate()
+
+        self.__model = Wav2Vec2ForCTC.from_pretrained(self.__path).to('cuda')
+        self.__processor = Wav2Vec2Processor.from_pretrained(self.__path)
+        self.__sample_rate = sample_rate
+
+    def __validate(self) -> None:
+        if not self.__path.exists():
+            not_exist_message: str = f'{self.__path.absolute()} does not exist'
+            raise ValueError(not_exist_message)
+
+        missing_necessary_files: list[str] = []
+        available_files: list[str] = []
+
+        for file in self.__path.iterdir():
+            available_files.append(file.name)
+
+        for file_ in self.__necessary_files:
+            if file_ not in available_files:
+                missing_necessary_files.append(file_)
+
+        if missing_necessary_files:
+            memory: list[str] = [
+                'Missing files:',
+                str(missing_necessary_files),
+                'get them at',
+                self.__link
+            ]
+
+            message: str = ' '.join(memory)
+
+            raise ValueError(message)
+
+    def recognize(self, data: BytesIO) -> str:
+        numpy_array: ndarray = librosa.load(data, sr=self.__sample_rate)[0]
+
+        inputs: BatchFeature = self.__processor(
+            numpy_array,
+            sampling_rate=self.__sample_rate,
+            return_tensors='pt',
+            padding=True,
+        )
+
+        inputs = {
+            key: value.to("cuda") for key, value in inputs.items()
+        }
+
+        with torch.no_grad():
+            outputs = self.__model(
+                **inputs
             )
 
             logits: torch.Tensor = outputs.logits
